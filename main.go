3c6c33b33254df30ff31a0f0d8e6c5978162e8b0
suzhumaga <sult.zhumagaliev07@gmail.com>
2025-02-04 17:59:14 +0500

    main file for test

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
-package main 
+package main
 
+import (
+	"fmt"
+	"io"
+	"os"
 
+	"a-library-for-others/csvlib"
+)
 
 func main() {
-	
+	file, err := os.Open("example.csv")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var csvparser csvlib.CSVParser = csvlib.MyCSVParser{}
+
+	for {
+		line, err := csvparser.ReadLine(file)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading line:", err)
+			return
+		}
+	}
 }
