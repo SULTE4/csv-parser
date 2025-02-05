2a32ebeb1546d83284e8aca43b5c3cf47f6174bb
SULTE4 <sult.zhumagaliev07@gmail.com>
2025-02-05 19:41:37 +0500

    made error respond

---
+++ csvlib/error.go
@@ -0,0 +1,18 @@
+package csvlib
+
+type CsvError struct {
+	Message string
+}
+
+func (e *CsvError) Error() string {
+	return e.Message
+}
+
+var (
+	ErrQuote      = &CsvError{Message: "excess or missing \" in quoted-field"}
+	ErrFieldCount = &CsvError{Message: "wrong number of fields"}
+)
+
+func NewCsvError(message string) error {
+	return &CsvError{Message: message}
+}
